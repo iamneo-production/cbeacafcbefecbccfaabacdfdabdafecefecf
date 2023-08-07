f4781694cf327141994bd4b69a2977721b35e8d0
Iamneo <techadmin@iamneo.ai>
2023-08-07 13:43:41 +0000

    Backup Commit

--- spring app/src/test/java/com/examly/springapp/SpringApplicationTests.java
+++ spring app/src/test/java/com/examly/springapp/SpringApplicationTests.java
@@ -14,19 +14,56 @@ public class SpringApplicationTests {
 
     ChromeOptions chromeOptions = new ChromeOptions();
     WebDriver driver = null;
-
+   
     @BeforeTest
     public void beforeTest() throws Exception
      {
-   // replace seleniumhost and port with correct values
-        driver = new RemoteWebDriver(new URL("http://<seleniumhost:port>/"), chromeOptions);
+     driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
         driver.manage().window().maximize();
     }
-
+   
+    @Test
+   //Checking the title of iamNeo (Home - iamneo)
+    public void iamNeo() throws InterruptedException 
+    {
+           driver.navigate().to("http://iamneo.ai");
+      String title =driver.getTitle();
+     // Assert.assertEquals(title, "Learning and assessment solution for Universities and Enterprises");
+    }
+    @Test
+   //Moving to FACEBOOK
+    public void nextPage() throws InterruptedException 
+    {
+        driver.navigate().to("https://www.facebook.com");
+      String title =driver.getTitle();
+     // Assert.assertEquals(title, "Facebook â€“ log in or sign up");
+   
+    }
+    @Test
+   //Back to iamNeo
+    public void backPage() throws InterruptedException 
+    {
+     driver.navigate().back();
+     String title =driver.getTitle();
+     // Assert.assertEquals(title, "Learning and assessment solution for Universities and Enterprises");
+   
+    }
+    @Test
+   //Current URL
+   public void currentURL() throws InterruptedException 
+   {
+      String title =driver.getCurrentUrl();
+      System.out.println(title);
+     // Assert.assertEquals(title, "");
+     driver.navigate().forward();
+     driver.navigate().refresh();
+   
+   }
+   
     @AfterTest
     public void afterTest() 
     {
-        driver.quit();
+     driver.quit();
     }
-
+   
 }
