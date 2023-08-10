43949ee86433ef27725e71e88780218a25667781
Iamneo <techadmin@iamneo.ai>
2023-08-10 06:32:07 +0000

    Backup Commit

--- Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
+++ Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
@@ -12,58 +12,53 @@ import org.testng.annotations.AfterTest;
 
 public class AppTest {
 
- ChromeOptions chromeOptions = new ChromeOptions();
- WebDriver driver = null;
+	ChromeOptions chromeOptions = new ChromeOptions();
+	WebDriver driver = null;
 
- @BeforeTest
- public void beforeTest() throws Exception
-  {
-  driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
-     driver.manage().window().maximize();
- }
+	@BeforeTest
+	public void beforeTest() throws Exception
+	 {
+		driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
+	}
 
- @Test
+	@Test
 //Checking the title of iamNeo (Home - iamneo)
- public void iamNeo() throws InterruptedException 
- {
-        driver.navigate().to("http://iamneo.ai");
-   String title =driver.getTitle();
-  // Assert.assertEquals(title, "Learning and assessment solution for Universities and Enterprises");
- }
- @Test
+	public void iamNeo() throws InterruptedException 
+	{
+
+		 String title ="Get your title";
+		Assert.assertEquals(title, "");
+	}
+	@Test
 //Moving to FACEBOOK
- public void nextPage() throws InterruptedException 
- {
-     driver.navigate().to("https://www.facebook.com");
-   String title =driver.getTitle();
-  // Assert.assertEquals(title, "Facebook â€“ log in or sign up");
+	public void nextPage() throws InterruptedException 
+	{
+		 	
+		 String title ="Get your title";
+		Assert.assertEquals(title, "");
 
- }
- @Test
+	}
+	@Test
 //Back to iamNeo
- public void backPage() throws InterruptedException 
- {
-  driver.navigate().back();
-  String title =driver.getTitle();
-  // Assert.assertEquals(title, "Learning and assessment solution for Universities and Enterprises");
+	public void backPage() throws InterruptedException 
+	{
+		 String title ="Get your title";
+		Assert.assertEquals(title, "");
 
- }
- @Test
+	}
+	@Test
 //Current URL
 public void currentURL() throws InterruptedException 
 {
-   String title =driver.getCurrentUrl();
-   System.out.println(title);
-  // Assert.assertEquals(title, "");
-  driver.navigate().forward();
-  driver.navigate().refresh();
+		 String title ="Get your title";
+		Assert.assertEquals(title, "");
 
 }
 
- @AfterTest
- public void afterTest() 
- {
-  driver.quit();
- }
+	@AfterTest
+	public void afterTest() 
+	{
+		driver.quit();
+	}
 
 }
